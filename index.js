1109cea40b2d82c57ee0641e0cbf10540195f791
Neowise <verve.neowise@gmail.com>
2022-07-01 15:11:30 +0500

    test

---
+++ index.js
@@ -0,0 +1,217 @@
+
+//Data types
+/*1 - Primitive
+1) number - 35486468
+2) string - matn '' " " ``
+3) null - nol
+4) boolean - true or false
+5) undefined - belgilanmagan
+6) BigInt -big integer
+7) symbol - unique key
+
+2 - Non - primitive -refence- object
+1) Array [ ]
+2) object { }
+3) function -funktiya
+*/
+
+
+//Number.parseInt
+//Number.parseFloat
+//Number.isInteger
+//toFixed
+
+
+
+
+
+/*
+  Math.round
+  Math.pow
+  Math.sqrt
+  Math.cbrt
+  Math.min
+  Math.max
+  Math.floor
+  Math.ceil
+  Math.abs
+  Math.random
+*/
+
+
+//comparision operators
+
+// ==, ===, <,> !==, <=, && and, ||
+
+//string
+
+// 'Bektemir', "Bektemir", `Bektemir`
+
+
+
+//string methods
+
+//1 length
+//indexOf
+//search
+//lastIndexOf
+//startsWith
+//endWith
+//toLowerCase
+//toUpperCase
+//charAt
+//charCodeAt
+//concat
+//repeat
+//replace
+//trim
+//slice
+//substring
+//substr
+//match
+//split
+//RegEx  /hello/i,  /hello/g
+
+
+//Implicit Conversion - automatic type conversion
+// let result;
+//let a = +'1233'
+
+// result = 7 - false;
+
+
+// result = 5 - true;
+
+
+// result = true + 5;
+
+
+
+//Explicit Conversion - manual type conversion
+
+// let result;
+// result = Boolean('');
+// console.log(result); // false
+
+// result = Boolean(0);
+// console.log(result); // false
+
+// result = Boolean(undefined);
+// console.log(result); // false
+
+// result = Boolean(null);
+// console.log(result); // false
+
+// result = Boolean(NaN);
+// console.log(result); // false
+
+// result = Boolean(false);
+// console.log(result); // false
+
+//Math.round = yaxlitlash
+//Math.pow= darajaga oshiradi
+//Math.sqrt = ildizdan chiqaradi
+//Math.cbrt = kubdan chiqaradi
+//Math.abs = moduldan chiqaradi
+//Math.floor = yaxlitlash
+//Math.ceil = yaxlitlash
+//Math.max= eng kattasini oladi
+//Math.min = eng kichigini oladi
+//Math.random = random
+
+//Number.parseInt
+//Number.parseFloat
+//toFixed()
+//isInteger
+
+
+//if else
+// var obhavo = 'kamalak'
+
+// if (obhavo == 'quyosh') {
+//   console.log('havo quyoshli');
+// }
+// else if (obhavo === 'yomgir') {
+//   console.log('havo yomgirli');
+// }
+// else if (obhavo == 'qor') {
+//   console.log('qor yogayapti');
+// }
+// else if (obhavo === 'bulut') {
+//   console.log('havo bulutli');
+// }
+// else {
+//   console.log('bunday havo turi mavjud emas');
+// }
+
+//switch case
+
+// var kun = 2
+
+// switch (kun) {
+//   case 1: console.log('dushanba'); break;
+//   case 2: console.log('seshanba'); break;
+//   case 3: console.log('chorshanba'); break;
+//   case 4: console.log('payshanba'); break;
+//   case 5: console.log('juma'); break;
+//   case 6: console.log('shanba'); break;
+//   case 7: console.log('yakshanba'); break;
+//   default: console.log('bunday kun yoq');
+// }
+
+//ternary operator
+
+// let obhavo = 'yomgir'
+
+// obhavo == 'yomgir' ? console.log('yomgir') : console.log('bunday obhavo turi yoq');
+
+
+// let num = Math.pow(5, 2)
+//function decloration, function expression, arrow function
+
+//fuction decloration
+
+// function bektemir(a) {
+//   return a
+// }
+// let result = bektemir('roza').toUpperCase()
+// console.log(result);
+
+//fuction expression
+// let ism = 'nodirbek'
+// const bektemir = function (name) {
+//   console.log(name);
+// }
+
+// bektemir(ism)
+// bektemir('roza')
+// bektemir('bektemir')
+
+//arrow function
+// const counter =(a)=> Math.pow(a,3)
+
+
+//for
+// let narx = 25
+// for (let i = 0; i > 30; i++) {
+//   console.log(i);
+// }
+
+//while
+// let i = 0
+// while (i < 10) {
+//   console.log(i);
+//   i++
+// }
+
+//do while
+// let i = 0
+// do {
+//   i++
+//   console.log(i);
+// } while (i > 10)
+
+//array
+let arr = ['Bobur', 'Shahzod', 'Bektemir']
+
+console.log(arr[0][2]);
